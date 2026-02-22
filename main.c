82a454f632d41395701820577fb42478cd082d2a
Andy Oldham <andyoldham1@hotmail.co.uk>
2026-02-22 17:25:28 +0000

    Second commit

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
  * File: main.c
  * Content: Main source file for alarm clock
  * using the AVR128DB28 microcontroller.
+ * Some hardware will be used for display and
+ * audio output.
  * Date: 22/2/2026
  *
  *********************************/
