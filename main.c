cc9ffd9baf735daab79aabfc94fe576a52064684
Andy Oldham <andyoldham1@hotmail.co.uk>
2026-02-22 17:22:36 +0000

    Initial commit of main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+/*============================;
+ *
+ * File: main.c
+ * Content: Main source file for alarm clock
+ * using the AVR128DB28 microcontroller.
+ * Date: 22/2/2026
+ *
+ *********************************/
+
+int main()
+{
+
+}
